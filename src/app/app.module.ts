d32d24799801aa932e487d3df953227fc550a9f5
heitor582 <heitorvieira.pvd@gmail.com>
2020-09-02 16:52:37 -0300

    Arrumando imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { MatSidenavModule } from  '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatCardModule } from '@angular/material/card';
-import { MatListModule } from '@angular/material/list';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -15,33 +11,27 @@ import { HomeComponent } from './pages/home/home.component';
 import { TodosComponent } from './pages/todos/todos.component';
 import { PostsComponent } from './pages/posts/posts.component';
 import { SidenavComponent } from './core/template/sidenav/sidenav.component';
-import { MaterialModule } from './core/material.module'
+import { MaterialModule } from './core/material.module';
 import { SidenavListComponent } from './core/template/sidenav/sidenav-list/sidenav-list.component';
 
-
-
 @NgModule({
-  declarations: [ ,
-
-     AppComponent,
-     HeaderComponent,
-     AlbumsComponent,
-     CommentsComponent,
-     HomeComponent,
-     TodosComponent,
-     PostsComponent,
-     SidenavComponent,
-     SidenavListComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    AlbumsComponent,
+    CommentsComponent,
+    HomeComponent,
+    TodosComponent,
+    PostsComponent,
+    SidenavComponent,
+    SidenavListComponent,
+  ],
   imports: [
     MaterialModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    MatToolbarModule,
-    MatCardModule,
-    MatSidenavModule,
-    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
