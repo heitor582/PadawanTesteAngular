11a8d757817739f143353616e54f49b82ad3cbab
heitor582 <heitorvieira.pvd@gmail.com>
2020-09-03 13:55:20 -0300

    albums table

--- src/app/core/services/postsService/posts.service.ts
+++ src/app/core/services/postsService/posts.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import PostsI from '../../models/posts.model';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PostsService {
-  baseUrl = 'http://jsonplaceholder.typicode.com/posts';
+  baseUrl = `${environment.API_SERVER}/posts`;
   constructor(private http: HttpClient) {}
   reqPosts(): Observable<PostsI[]> {
     return this.http.get<PostsI[]>(this.baseUrl);
--- src/app/core/services/todoService/todo.service.ts
+++ src/app/core/services/todoService/todo.service.ts
@@ -2,11 +2,12 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import TodoI from '../../models/todo.model';
+import { environment } from '../../../../environments/environment';
 @Injectable({
   providedIn: 'root',
 })
 export class TodoService {
-  baseUrl = 'http://jsonplaceholder.typicode.com/todos';
+  baseUrl = `${environment.API_SERVER}/todos`;
   constructor(private http: HttpClient) {}
   reqTodos(): Observable<TodoI[]> {
     return this.http.get<TodoI[]>(this.baseUrl);
